5cbb3be088bea274ed756f7d4714456e054eea9e
SvetlanaLukyano <gaechkadale@yandex.ru>
2023-12-10 20:11:23 +0300

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+
+
+public class Main {
+        public static void main(String[] args) {
+
+            public void getArrayAsInteger(int[] arr){
+                for (int i : arr) {
+                    System.out.println(i);
+                }
+            }
+
+                public void getArrayAsString(String[] arr) {
+                    for (int i : arr) {
+                        System.out.println(i);
+                    }
+                }
+            }
+
+
+
