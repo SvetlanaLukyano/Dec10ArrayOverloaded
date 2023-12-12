e00af8735541d2462768f7d755acdc2e9a20bcf9
SvetlanaLukyano <150173467+SvetlanaLukyano@users.noreply.github.com>
2023-12-12 19:36:40 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,18 +3,23 @@
 public class Main {
         public static void main(String[] args) {
 
-            public void getArrayAsInteger(int[] arr){
+        }
+
+            public void getArrayAsInteger(int[] arr)
+        {
                 for (int i : arr) {
                     System.out.println(i);
                 }
             }
 
-                public void getArrayAsString(String[] arr) {
+                public void getArrayAsString(String[] arr) 
+            {
                     for (int i : arr) {
                         System.out.println(i);
                     }
                 }
-            }
+           
+}
 
 
 
